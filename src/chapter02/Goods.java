0adaa8de115ce186db373579632fc5321f1cae7f
bsjxer <bsjxer@gmail.com>
2016-09-12 16:23:17 +0900

    .

---
+++ src/chapter02/Goods.java
@@ -0,0 +1,40 @@
+package chapter02;
+
+public class Goods {
+	private String name;
+	private int price;
+	private int countStock;
+	private int countSold;
+	
+	// 정보 은닉화. 내부 데이터는 private 설정 하고 메소드를 통해 접근한다. 메소드를 통해 접근하면 데이터를 보호 할 수 있다.
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		// 정보 보호
+		if( price < 0 ) {
+			price = 0;
+		}
+		this.price = price;
+	}
+	public int getCountStock() {
+		return countStock;
+	}
+	public void setCountStock(int countStock) {
+		this.countStock = countStock;
+	}
+	public int getCountSold() {
+		return countSold;
+	}
+	public void setCountSold(int countSold) {
+		this.countSold = countSold;
+	}
+	
+	
+}
---
+++ src/mypackage/GoodsApp.java
@@ -0,0 +1,21 @@
+package mypackage;
+
+import chapter02.Goods;
+
+public class GoodsApp {
+
+	public static void main(String[] args) {
+		Goods goods = new Goods();
+		goods.setName( "Nikon" );
+		// goods.Price = -1;
+		goods.setPrice( -1 );
+		goods.setCountStock( 50 );
+		goods.setCountSold( 10 );
+		
+		System.out.println(
+				goods.getName() + ":" +
+				goods.getPrice() + ":" + 
+				goods.getCountStock() + ":" + 
+				goods.getCountSold() );
+	}
+}
