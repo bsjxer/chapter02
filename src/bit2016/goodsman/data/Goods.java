ef11356006d3578678c70d49f7f831eee219efb9
bsjxer <bsjxer@gmail.com>
2016-09-19 14:06:04 +0900

    160919

--- src/bit2016/goodsman/data/Goods.java
+++ src/bit2016/goodsman/data/Goods.java
@@ -1,6 +1,13 @@
 package bit2016.goodsman.data;
 
 public class Goods {
+	public Goods() {
+		System.out.println( "생성자 호출" );
+		count++;
+	}
+	
+	public static int count;
+	
 	private String name;
 	private int price;
 	private int countStock;
@@ -10,9 +17,13 @@ public class Goods {
 	public String getName() {
 		return name;
 	}
+	
+	//return 값이 없고 매개변수 (parameter)가 있는 메소드
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	//return 값이 있고 매개변수 (parameter)가 없는 메소드
 	public int getPrice() {
 		return price;
 	}
@@ -34,6 +45,22 @@ public class Goods {
 	}
 	public void setCountSold(int countSold) {
 		this.countSold = countSold;
+	}
+	
+	//return 값과 매개변수 (parameter)가 없는 메소드
+	public void showInfo() {
+		System.out.println(
+				"name=" + name + 
+				", price=" + price +
+				", countStock = " + countStock +
+				", countSold= " + countSold );
+	}
+	
+	//return 값이 있고 매개변수(parameter)도 있는 메소드
+	public int calcDiscountPrice( double rate ) {
+		int discountPrice = (int)(price * rate);
+		return discountPrice;
+		//return (int)(price * rate);  이것만 해줘도 가능
 	}
 	
 	
--- src/bit2016/goodsman/main/GoodsApp.java
+++ src/bit2016/goodsman/main/GoodsApp.java
@@ -8,14 +8,24 @@ public class GoodsApp {
 		Goods goods = new Goods();
 		goods.setName( "Nikon" );
 		// goods.Price = -1;
-		goods.setPrice( -1 );
+		goods.setPrice( 2000 );
 		goods.setCountStock( 50 );
 		goods.setCountSold( 10 );
-		
+		System.out.println( Goods.count );
 		System.out.println(
 				goods.getName() + ":" +
 				goods.getPrice() + ":" + 
 				goods.getCountStock() + ":" + 
 				goods.getCountSold() );
+		
+		Goods goods2 = new Goods();
+		System.out.println( Goods.count );
+		
+		goods.showInfo();
+		
+		int discountPrice = (int)(0.1 * goods.getPrice());
+//		int discountPrice = goods.calcDiscountPrice( 0.1 );
+//		System.out.println( discountPrice );
+		
 	}
 }
